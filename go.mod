8e6f67f65bcf74c2cbaaa0c121a0513593b92996
Michelangelo <michelangelo@morrillo.it>
2020-06-22 15:19:59 +0200

    improvements

--- go.mod
+++ go.mod
@@ -4,10 +4,12 @@ go 1.14
 
 require (
 	github.com/briandowns/spinner v1.11.1
-	github.com/google/go-github/v31 v31.0.1-0.20200522014904-a37dd2dda868
-	github.com/imdario/mergo v0.3.9
+	github.com/fatih/structs v1.1.0
+	github.com/google/go-github/v31 v31.0.0
 	github.com/jinzhu/copier v0.0.0-20190924061706-b57f9002281a
+	github.com/mitchellh/mapstructure v1.3.2
+	github.com/pkg/errors v0.9.1
 	github.com/urfave/cli/v2 v2.2.0
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
-	gopkg.in/yaml.v2 v2.2.8
+	gopkg.in/yaml.v2 v2.3.0
 )
--- utils.go
+++ utils.go
@@ -3,8 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"reflect"
 	"regexp"
 	"strings"
+
+	"github.com/fatih/structs"
+	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
 )
 
 func askForConfirmation(message string) bool {
@@ -42,4 +47,44 @@ func containsString(slice []string, element string) bool {
 func slug(s string) string {
 	re := regexp.MustCompile("[^a-z0-9]+")
 	return strings.Trim(re.ReplaceAllString(strings.ToLower(s), "-"), "-")
+}
+
+func mergeOverwrite(to, from, dst interface{}) error {
+	toMap := structs.Map(to)
+	toStruct := structs.New(to)
+	fromMap := structs.Map(from)
+	fromStruct := structs.New(from)
+	for k, v := range fromMap {
+		_, ok := toMap[k]
+		if !ok {
+			return errors.Errorf("no key: %s", k)
+		}
+		toField := toStruct.Field(k)
+		fromField := fromStruct.Field(k)
+		if overwriteable(toField, fromField) {
+			toMap[k] = v
+		}
+	}
+	if err := mapstructure.Decode(toMap, dst); err != nil {
+		return errors.Wrap(err, "faield to decode")
+	}
+	return nil
+}
+
+func overwriteable(to, from *structs.Field) bool {
+	if to.Kind().String() == "bool" {
+		return true
+	}
+	if reflect.TypeOf(to).Kind().String() == "struct" {
+		return false
+	}
+	switch {
+	case to.IsZero() && !from.IsZero():
+		return true
+	case !to.IsZero() && !from.IsZero():
+		return true
+	case !to.IsZero() && from.IsZero():
+		return false
+	}
+	return true
 }
